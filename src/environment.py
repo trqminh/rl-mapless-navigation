349b2582fdffab5147e6fbfa5119407af771da0b
trqminh <1612374@student.hcmus.edu.vn>
2020-07-11 16:56:23 +0700

    modify goal generation for training

--- src/environment.py
+++ src/environment.py
@@ -31,6 +31,7 @@ class Env():
         self.goal = rospy.ServiceProxy('/gazebo/spawn_sdf_model', SpawnModel)
         self.del_model = rospy.ServiceProxy('/gazebo/delete_model', DeleteModel)
         self.past_distance = 0.
+        self.test_goals = [(0.,3.), (3.,1.), (0.,0.), (-2.,-1.), (-0.5, -3.2)]
         if is_training:
             self.threshold_arrive = 0.2
         else:
@@ -137,8 +138,12 @@ class Env():
                 target = SpawnModel
                 target.model_name = 'target'  # the same with sdf name
                 target.model_xml = goal_urdf
-                self.goal_position.position.x = random.uniform(-3.6, 3.6)
-                self.goal_position.position.y = random.uniform(-3.6, 3.6)
+                while True:
+                    x, y = random.uniform(-3.2, 3.2), random.uniform(-3.2, 3.2)
+                    if abs(x) > 1. or abs(y) > 1.:
+                        break
+                self.goal_position.position.x = x
+                self.goal_position.position.y = y
                 self.goal(target.model_name, target.model_xml, 'namespace', self.goal_position, 'world')
             except (rospy.ServiceException) as e:
                 print("/gazebo/failed to build the target")
@@ -193,8 +198,12 @@ class Env():
             target = SpawnModel
             target.model_name = 'target'  # the same with sdf name
             target.model_xml = goal_urdf
-            self.goal_position.position.x = random.uniform(-3.6, 3.6)
-            self.goal_position.position.y = random.uniform(-3.6, 3.6)
+            while True:
+                x, y = random.uniform(-3.2, 3.2), random.uniform(-3.2, 3.2)
+                if abs(x) > 1. or abs(y) > 1.:
+                    break
+            self.goal_position.position.x = x
+            self.goal_position.position.y = y
 
             # if -0.3 < self.goal_position.position.x < 0.3 and -0.3 < self.goal_position.position.y < 0.3:
             #     self.goal_position.position.x += 1
